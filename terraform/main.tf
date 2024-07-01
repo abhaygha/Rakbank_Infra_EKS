02b979b5c999f9d1b0eea6ecb1ba8b7c415b7be6
abhay <abhayruby1019@gmail.com>
2024-07-01 16:31:18 +0530

    added terraform script

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,25 +2,106 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  name   = "my-vpc"
-  cidr   = "10.0.0.0/16"
+# Define an IAM role for the EKS cluster control plane
+resource "aws_iam_role" "eks_cluster" {
+  name = "eks-cluster"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
 }
 
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "my-cluster"
-  cluster_version = "1.21"
-  vpc_id          = module.vpc.vpc_id
-  subnet_ids      = ["subnet-002f2e053f0ef7400"]  # Provided subnet
+# Attach AmazonEKSClusterPolicy to the IAM role created for EKS cluster
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster.name
+}
 
-  node_groups = {
-    my_group = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-      instance_type    = "m5.large"
-    }
+# Attach AmazonEKSServicePolicy to the IAM role created for EKS cluster
+resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks_cluster.name
+}
+
+# Create an EKS cluster
+resource "aws_eks_cluster" "aws_eks" {
+  name     = "eks_cluster_demo"
+  role_arn = aws_iam_role.eks_cluster.arn
+
+  vpc_config {
+    subnet_ids = ["subnet-002f2e053f0ef7400", "subnet-0c77e502f06e60f29"]  # Your provided subnets
   }
+
+  tags = {
+    Name = "EKS_demo"
+  }
+}
+
+# Define an IAM role for EKS worker nodes
+resource "aws_iam_role" "eks_nodes" {
+  name = "eks-node-group-demo"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+# Attach AmazonEKSWorkerNodePolicy to the IAM role created for EKS worker nodes
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+# Attach AmazonEKS_CNI_Policy to the IAM role created for EKS worker nodes
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+# Attach AmazonEC2ContainerRegistryReadOnly to the IAM role created for EKS worker nodes
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+# Create an EKS node group
+resource "aws_eks_node_group" "node" {
+  cluster_name    = aws_eks_cluster.aws_eks.name
+  node_group_name = "node_demo"
+  node_role_arn   = aws_iam_role.eks_nodes.arn
+  subnet_ids      = ["subnet-002f2e053f0ef7400", "subnet-0c77e502f06e60f29"]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,5 @@
-output "cluster_name" {
-  value = module.eks.cluster_id
-}
-
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
-output "cluster_security_group_id" {
-  value = module.eks.cluster_security_group_id
+// Define an output block to expose the name of the EKS cluster
+output "eks_cluster_name" {
+  // Set the value of the output to be the name of the EKS cluster
+  value = aws_eks_cluster.aws_eks.name
 }
