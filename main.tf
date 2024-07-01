725d7a2596aa634119fff2d32ea677a559c0cdc4
abhay <abhayruby1019@gmail.com>
2024-07-01 15:34:42 +0530

    workflow updated

--- main.tf
+++ main.tf
@@ -12,16 +12,16 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "my-cluster"
   cluster_version = "1.21"
-  subnets         = ["subnet-002f2e053f0ef7400"]  # Provided subnet
-  vpc_id          = "vpc-05b315a793fddc7f0"       # Provided VPC ID
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = ["subnet-002f2e053f0ef7400"]  # Provided subnet
 
-  node_groups = {
-    my_group = {
+  node_groups = [
+    {
+      name          = "my-group"
+      instance_type = "m5.large"
       desired_capacity = 2
       max_capacity     = 3
       min_capacity     = 1
-
-      instance_type = "m5.large"
     }
-  }
+  ]
 }
